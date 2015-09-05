4d98c0581ce1fd8bb2a5644acc84a01959ddeb69
deepshine <fish83814@yahoo.com.tw>
2015-09-05 23:38:39 +0800

    try rotation animation

--- RollingPigs/app/src/main/java/hackathon/nctucs/rollingpigs/elements/Node.java
+++ RollingPigs/app/src/main/java/hackathon/nctucs/rollingpigs/elements/Node.java
@@ -16,76 +16,79 @@ import java.util.Map;
 
 /**
  * Created by Shamrock on 2015/9/5.
+ * Modified by Deepshine.
  */
-public class Node {
+public class Node{
 
 
-    public boolean valid;
-    private int id, onSId, color;
-    private int radius;
-    private int src;
-    public ImageView img;
+	public boolean valid;
+	private int id, onSId, color;
+	private int radius;
+	private int src;
+	public ImageView img;
 
 
-    public Node( int _id , int _color , int _onSId , int _radius , int _src ){
+	public Node(int _id, int _color, int _onSId, int _radius, int _src){
 
-        id = _id;
-        color = _color;
-        onSId = _onSId;
-        src = _src;
-        radius = _radius;
+		id = _id;
+		color = _color;
+		onSId = _onSId;
+		src = _src;
+		radius = _radius;
 
-    }
-
-
-    public int getOnSId(){
-        return onSId;
-    }
-
-    public int getId(){
-        return id;
-    }
-    public int getSrc(){
-        return src;
-    }
-    public double getRadius(){
-        return radius;
-    }
-    public int getColor(){
-        return color;
-    }
-    public void animate( int circleX , int circleY , int circleR , int fromX , int fromY , int targetX , int targetY  ){
+	}
 
 
-        final RectF rect = new RectF( circleX-circleR-radius , circleY-circleR-radius , circleX+circleR-radius , circleY+circleR-radius );
-        Log.e( "" + (circleX - circleR) , "" + (circleX+circleR));
-        Log.e( "" + (circleY - circleR) , "" + (circleY+circleR));
-        Path path = new Path();
-        int[] deg = degreeRange( circleX , circleY , fromX , fromY , targetX , targetY );
+	public int getOnSId(){
+		return onSId;
+	}
 
-        Log.e( "!!!!" , circleX + " " + circleY + " " + fromX + " " + fromY + " " + targetX + " " + targetY );
+	public int getId(){
+		return id;
+	}
 
-        Log.e( deg[0] + " ", deg[1]  + " ");
+	public int getSrc(){
+		return src;
+	}
 
+	public double getRadius(){
+		return radius;
+	}
 
+	public int getColor(){
+		return color;
+	}
 
-        path.addArc( rect , deg[0]  , (deg[1]-deg[0] + 360) % 360 );
-        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(img, View.X, View.Y, path);
-        objectAnimator.setDuration( 1000 );
+	public void animate(int circleX, int circleY, int circleR, int fromX, int fromY, int targetX, int targetY){
+		final RectF rect = new RectF(circleX-circleR-radius, circleY-circleR-radius, circleX+circleR-radius, circleY+circleR-radius);
+		Log.e(""+(circleX-circleR), ""+(circleX+circleR));
+		Log.e(""+(circleY-circleR), ""+(circleY+circleR));
+		Path path = new Path();
+		int[] deg = degreeRange(circleX, circleY, fromX, fromY, targetX, targetY);
 
-        objectAnimator.start();
+		Log.e("!!!!", circleX+" "+circleY+" "+fromX+" "+fromY+" "+targetX+" "+targetY);
 
+		Log.e(deg[0]+" ", deg[1]+" ");
 
+		path.addArc(rect, deg[0], (deg[1]-deg[0]+360)%360);
+		ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(img, View.X, View.Y, path);
+		objectAnimator.setDuration(1000);
 
+		Animation animation = new RotateAnimation(0.0f, 360.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+		animation.setRepeatCount(0);
+		animation.setDuration(1000);
 
-    }
-    public void setOnSId( int _sid ){
-        onSId = _sid;
-    }
+		img.startAnimation(animation);
+		objectAnimator.start();
+	}
+
+	public void setOnSId(int _sid){
+		onSId = _sid;
+	}
 
 	int[] degreeRange(int centerX, int centerY, int startX, int startY, int targetX, int targetY){
-		int v1X = startX - centerX, v1Y = startY - centerY;
-		int v2X = targetX - centerX, v2Y = targetY - centerY;
+		int v1X = startX-centerX, v1Y = startY-centerY;
+		int v2X = targetX-centerX, v2Y = targetY-centerY;
 		return new int[]{(int)(Math.atan2(v1Y, v1X)*180/Math.PI), (int)(Math.atan2(v2Y, v2X)*180/Math.PI)};
 	}
 
